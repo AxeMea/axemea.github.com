379ad89c62e0c0a76c9653418ad581050962d923
zheng yu <286915571@qq.com>
2014-09-16 17:19:00 +0800

    update

--- _plugins/monthly_archive_plugin.rb
+++ _plugins/monthly_archive_plugin.rb
@@ -3,87 +3,102 @@
 # Shigeya Suzuki, November 2013
 # Copyright notice (MIT License) attached at the end of this file
 #
+
 #
 # This code is based on the following works:
-# https://gist.github.com/ilkka/707909
-# https://gist.github.com/ilkka/707020
-# https://gist.github.com/nlindley/6409459
+#   https://gist.github.com/ilkka/707909
+#   https://gist.github.com/ilkka/707020
+#   https://gist.github.com/nlindley/6409459
 #
+
 #
 # Archive will be written as #{archive_path}/#{year}/#{month}/index.html
 # archive_path can be configured in 'path' key in 'monthly_archive' of
 # site configuration file. 'path' is default null.
 #
+
 module Jekyll
-module MonthlyArchiveUtil
-def self.archive_base(site)
-site.config['monthly_archive'] && site.config['monthly_archive']['path'] || ''
-end
-end
-# Generator class invoked from Jekyll
-class MonthlyArchiveGenerator < Generator
-def generate(site)
-posts_group_by_year_and_month(site).each do |ym, list|
-site.pages << MonthlyArchivePage.new(site, MonthlyArchiveUtil.archive_base(site),
-ym[0], ym[1], list)
-end
-end
-def posts_group_by_year_and_month(site)
-site.posts.each.group_by { |post| [post.date.year, post.date.month] }
-end
-end
-# Actual page instances
-class MonthlyArchivePage < Page
-ATTRIBUTES_FOR_LIQUID = %w[
-year,
-month,
-date,
-content
-]
-def initialize(site, dir, year, month, posts)
-@site = site
-@dir = dir
-@year = year
-@month = month
-@archive_dir_name = '%04d/%02d' % [year, month]
-@date = Date.new(@year, @month)
-@layout = site.config['monthly_archive'] && site.config['monthly_archive']['layout'] || 'monthly_archive'
-self.ext = '.html'
-self.basename = 'index'
-self.content = <<-EOS
+
+  module MonthlyArchiveUtil
+    def self.archive_base(site)
+      site.config['monthly_archive'] && site.config['monthly_archive']['path'] || ''
+    end
+  end
+
+  # Generator class invoked from Jekyll
+  class MonthlyArchiveGenerator < Generator
+    def generate(site)
+      posts_group_by_year_and_month(site).each do |ym, list|
+        site.pages << MonthlyArchivePage.new(site, MonthlyArchiveUtil.archive_base(site),
+                                             ym[0], ym[1], list)
+      end
+    end
+
+    def posts_group_by_year_and_month(site)
+      site.posts.each.group_by { |post| [post.date.year, post.date.month] }
+    end
+
+  end
+
+  # Actual page instances
+  class MonthlyArchivePage < Page
+
+    ATTRIBUTES_FOR_LIQUID = %w[
+      year,
+      month,
+      date,
+      content
+    ]
+
+    def initialize(site, dir, year, month, posts)
+      @site = site
+      @dir = dir
+      @year = year
+      @month = month
+      @archive_dir_name = '%04d/%02d' % [year, month]
+      @date = Date.new(@year, @month)
+      @layout =  site.config['monthly_archive'] && site.config['monthly_archive']['layout'] || 'monthly_archive'
+      self.ext = '.html'
+      self.basename = 'index'
+      self.content = <<-EOS
 {% for post in page.posts %}<li><a href="{{ post.url }}"><span>{{ post.title }}<span></a></li>
 {% endfor %}
-EOS
-self.data = {
-'layout' => @layout,
-'type' => 'archive',
-'title' => "Monthly archive for #{@year}/#{@month}",
-'posts' => posts,
-'url' => File.join('/',
-MonthlyArchiveUtil.archive_base(site),
-@archive_dir_name, 'index.html')
-}
+      EOS
+      self.data = {
+          'layout' => @layout,
+          'type' => 'archive',
+          'title' => "Monthly archive for #{@year}/#{@month}",
+          'posts' => posts,
+          'url' => File.join('/',
+                     MonthlyArchiveUtil.archive_base(site),
+                     @archive_dir_name, 'index.html')
+      }
+    end
+
+    def render(layouts, site_payload)
+      payload = {
+          'page' => self.to_liquid,
+          'paginator' => pager.to_liquid
+      }.merge(site_payload)
+      do_layout(payload, layouts)
+    end
+
+    def to_liquid(attr = nil)
+      self.data.merge({
+                               'content' => self.content,
+                               'date' => @date,
+                               'month' => @month,
+                               'year' => @year
+                           })
+    end
+
+    def destination(dest)
+      File.join('/', dest, @dir, @archive_dir_name, 'index.html')
+    end
+
+  end
 end
-def render(layouts, site_payload)
-payload = {
-'page' => self.to_liquid,
-'paginator' => pager.to_liquid
-}.merge(site_payload)
-do_layout(payload, layouts)
-end
-def to_liquid(attr = nil)
-self.data.merge({
-'content' => self.content,
-'date' => @date,
-'month' => @month,
-'year' => @year
-})
-end
-def destination(dest)
-File.join('/', dest, @dir, @archive_dir_name, 'index.html')
-end
-end
-end
+
 # The MIT License (MIT)
 #
 # Copyright (c) 2013 Shigeya Suzuki
